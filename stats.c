93cd9f949cd18b72f115cd569e5137026fb48915
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-13 22:07:14 +0200

    Update stats.c

--- stats.c
+++ stats.c
@@ -7,7 +7,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     
     if (numberset[]== 0 && setlength ==0)
-    s.average = NaN;
+    s.average = NAN;
     
     int sum =0;
     for ( int a = 0 ; a < = setlength ; a++)
@@ -22,19 +22,19 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     
     for (int i = 1; i < = setlength; i++)
     {
-      min_no = min(min_no, numberset[i]);
+      min_no = std::min(min_no, numberset[i]);
     }
     s.min = min_no;
     
     int max_no = numberset[0];
     for (int j =1; j < = setlength; j++)
     {
-        max_no = max(max_no, numberset[j]);
+        max_no = std::max(max_no, numberset[j]);
     }
     s.max = max_no;
 }
 
-int check_and_alert(float maxThreshold, int alerter_funcptr alerters[], struct Stats computedStats)
+int check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
