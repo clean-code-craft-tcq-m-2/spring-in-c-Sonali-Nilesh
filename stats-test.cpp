fe644330e00ddc740ddc1840d727f08b83297b32
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-13 22:07:18 +0200

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -20,7 +20,7 @@ TEST_CASE("average is NaN for empty array") {
     struct Stats computedStats = compute_statistics(0, 0);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
-    REQUIRE(computedStats.average == NaN);
+    REQUIRE(computedStats.average == NAN);
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
 }
@@ -28,7 +28,7 @@ TEST_CASE("average is NaN for empty array") {
 TEST_CASE("raises alerts when max is greater than threshold") {
     // create additional .c and .h files
     // containing the emailAlerter, ledAlerter functions
-   int alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
+   alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
