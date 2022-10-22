edccbc98d22b22afc1a5ee45b1ca3767e0894289
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-22 19:14:50 +0200

    Update stats.c

--- stats.c
+++ stats.c
@@ -4,53 +4,6 @@
 #include <stdlib.h>
 
 
-struct Stats compute_statistics(const float* numberset, int setlength) 
-{
-   struct s
-   {
-      
-    float average = 0.0;
-    float min = 0.0;
-    float max = 0.0;
-   };
-   
-   struct s calc;
-   calc.average = 0;
-    calc.min = 0;
-    calc.max = 0;
-
-    if (numberset == 0 && setlength ==0)
-    {
-    calc.average = NAN;
-    }
-    
-    float sum =0;
-    for ( int a = 0 ; a <= setlength ; a++)
-    {
-        sum = sum + numberset[a] ;
-    }
-   calc.average = sum /setlength;
-    
-    
-    float min_no = numberset[0];
-    
-    for (int i = 1; i <= setlength; i++)
-    {
-      min_no = std::min(min_no, numberset[i]);
-    }
-    calc.min = min_no;
-    
-   
-    float max_no = numberset[0];
-    for (int j =1; j <= setlength; j++)
-    {
-        max_no = std::max(max_no, numberset[j]);
-    }
-    calc.max = max_no;
-   
-   return calc;
-  
-}
 
 
 /*
