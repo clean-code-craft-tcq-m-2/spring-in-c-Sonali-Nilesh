99a81e08a54981b12fe4a20e6c1743c1a63cee73
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-22 23:15:19 +0200

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,7 +3,7 @@
  
 
 typedef void (*alerter_funcptr)();
-int check_and_alert(float maxThreshold, alerter_funcptr alerters[], float max_no);
+int check_and_alert(float maxThreshold, alerter_funcptr alerters[emailAlerter, ledAlerter], float max_no);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
