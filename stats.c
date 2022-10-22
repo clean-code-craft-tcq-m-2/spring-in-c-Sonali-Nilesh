76871070bd652ae5a310903bde4a201954578dda
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-23 00:50:54 +0200

    Update stats.c

--- stats.c
+++ stats.c
@@ -4,14 +4,7 @@
 #include <stdlib.h>
 #include "stats.h"
 
- int check_and_alert(float maxT, int alert[], float maxN)
-{
-     if (maxN > maxT)
-     {
-      alert[0] =1;
-      alert[1] =1;
-     
-     }
+ 
 
 
 
