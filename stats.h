0d6cc490448cdd1f390c61be4e83c1e32bb3ed0e
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-22 20:18:19 +0200

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,10 +1,15 @@
 
 //struct Stats compute_statistics(const float* numberset, int setlength);
-   struct calcu
+   Class calculation
     {
-       float average = 0.0;
+    public:
+    float numberset[] = {1.5, 8.9, 3.2, 4.5};
+    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    compute_statistics(numberset, setlength);
+    float average1 = 0.0;
     float min = 0.0;
     float max = 0.0;
+    
     
     };
 
