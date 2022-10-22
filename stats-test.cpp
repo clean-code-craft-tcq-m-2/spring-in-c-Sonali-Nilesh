0543400dccad1694e4a96d7f3bfea61bcceb2e36
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-22 20:33:13 +0200

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -16,7 +16,8 @@
 
 TEST_CASE("reports average, minimum and maximum") 
 {
- 
+  float numberset[] = {1.5, 8.9, 3.2, 4.5};
+ int setlength = sizeof(numberset) / sizeof(numberset[0]);
     
     struct s calc;
    
@@ -46,8 +47,7 @@ TEST_CASE("reports average, minimum and maximum")
         max_no = std::max(max_no, numberset[j]);
     }
     calc.max = max_no;
-   float numberset[] = {1.5, 8.9, 3.2, 4.5};
- int setlength = sizeof(numberset) / sizeof(numberset[0]);
+  
  
     float epsilon = 0.001;
  
