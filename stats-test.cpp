c2bde999f2745eadc1661e29b9d039b08cddce49
Sonali-Nilesh <115540057+Sonali-Nilesh@users.noreply.github.com>
2022-10-23 00:43:33 +0200

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -126,6 +126,18 @@ extern int ledAlertCallCount;
 
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, max_no);
+ 
+ int check_and_alert(float maxT, int alert[], float maxN)
+{
+     if (maxN > maxT)
+     {
+      alert[0] =1;
+      alert[1] =1;
+     
+     }
+   
+ return 0; 
+}
      emailAlertCallCount = alerters[0];
  ledAlertCallCount = alerters[1];;
     // need a way to check if both emailAlerter, ledAlerter were called
